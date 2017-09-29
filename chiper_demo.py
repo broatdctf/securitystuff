37a234e3e7db7183c4e3c467db5d3a6e62ef9cbd
broatdctf <32387787+broatdctf@users.noreply.github.com>
2017-09-29 14:02:20 +0300

    Update chiper_demo.py

--- chiper_demo.py
+++ chiper_demo.py
@@ -15,8 +15,8 @@ def decrypt(key, encryped):
     return ''.join(msg)
 
 if __name__ == '__main__':
-    key = 'fossil'
-    msg = 'I have always believed the you can recover this basic flag from my so basic encryption. And actually you found it in your own way. I appreciate that. Well My friend this is your key: >>>>>>>>>> DCTF{a752958e5efbaa808d8c173c9d21208f06927d1a0929d2f3068ea083c692513b} <<<<<<<<<<<<. Now I will just sing a song in order to prelong the length of this message. A B C D E F G Come and sing along with me H I J K L M N O P Tell me what you want to be Q R S T U V W X Y and ZNow I know my ABCs Next time won t you sing with me A B C D E F G H I J K L M N O P Q R S T U V W X Y and Z Now I know my ABCs Next time won t you sing with me! ONE MORE TIME! A B C D E F G Come and sing along with me H I J K L M N O P Tell me what you want to be Q R S T U V W X Y and ZNow I know my ABCs Next time won t you sing with me A B C D E F G H I J K L M N O P Q R S T U V W X Y and Z Now I know my ABCs Next time won t you sing with me!'
+    key = 'thispasswordissecret'
+    msg = 'I have always believed the you can recover this basic flag from my so basic encryption. And actually you found it in your own way. I appreciate that. Well My friend this is your key: >>>>>>>>> This is a big secret <<<<<<<<<<<<. Now I will just sing a song in order to prelong the length of this message. A B C D E F G Come and sing along with me H I J K L M N O P Tell me what you want to be Q R S T U V W X Y and ZNow I know my ABCs Next time won t you sing with me A B C D E F G H I J K L M N O P Q R S T U V W X Y and Z Now I know my ABCs Next time won t you sing with me! ONE MORE TIME! A B C D E F G Come and sing along with me H I J K L M N O P Tell me what you want to be Q R S T U V W X Y and ZNow I know my ABCs Next time won t you sing with me A B C D E F G H I J K L M N O P Q R S T U V W X Y and Z Now I know my ABCs Next time won t you sing with me!'
     encrypted = encrypt(key, msg)
     decrypted = decrypt(key, encrypted)
 
