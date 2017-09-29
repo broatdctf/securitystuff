74b53bb567efb0a3fc62adb433fc7e89bd2456cf
broatdctf <32387787+broatdctf@users.noreply.github.com>
2017-09-29 13:57:55 +0300

    Add files via upload

---
+++ chiper_demo.py
@@ -0,0 +1,26 @@
+def encrypt(key, msg):
+    encryped = []
+    for i, c in enumerate(msg):
+        key_c = ord(key[i % len(key)])
+        msg_c = ord(c)
+        encryped.append(chr((msg_c + key_c) % 127))
+    return ''.join(encryped)
+
+def decrypt(key, encryped):
+    msg = []
+    for i, c in enumerate(encryped):
+        key_c = ord(key[i % len(key)])
+        enc_c = ord(c)
+        msg.append(chr((enc_c - key_c) % 127))
+    return ''.join(msg)
+
+if __name__ == '__main__':
+    key = 'fossil'
+    msg = 'I have always believed the you can recover this basic flag from my so basic encryption. And actually you found it in your own way. I appreciate that. Well My friend this is your key: >>>>>>>>>> DCTF{a752958e5efbaa808d8c173c9d21208f06927d1a0929d2f3068ea083c692513b} <<<<<<<<<<<<. Now I will just sing a song in order to prelong the length of this message. A B C D E F G Come and sing along with me H I J K L M N O P Tell me what you want to be Q R S T U V W X Y and ZNow I know my ABCs Next time won t you sing with me A B C D E F G H I J K L M N O P Q R S T U V W X Y and Z Now I know my ABCs Next time won t you sing with me! ONE MORE TIME! A B C D E F G Come and sing along with me H I J K L M N O P Tell me what you want to be Q R S T U V W X Y and ZNow I know my ABCs Next time won t you sing with me A B C D E F G H I J K L M N O P Q R S T U V W X Y and Z Now I know my ABCs Next time won t you sing with me!'
+    encrypted = encrypt(key, msg)
+    decrypted = decrypt(key, encrypted)
+
+    print 'Message:', repr(msg)
+    print 'Key:', repr(key)
+    print 'Encrypted:', repr(encrypted)
+    print 'Decrypted:', repr(decrypted)
